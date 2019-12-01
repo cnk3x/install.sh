3b0f51e2557623c0acfd4437e7d973a28c245290
shu <shu@aryao.cn>
2019-12-02 05:54:17 +0800

    -

--- upload-ssl-certs.sh
+++ upload-ssl-certs.sh
@@ -11,6 +11,7 @@ fi
 for server in ${@:2}; do
     ssh root@${server} mkdir -p /etc/caddy/ssl
     scp ${domain}.key ${domain}.pem root@${server}:/etc/caddy/ssl/
+    ssh root@${server} chmod 0666 /etc/caddy/ssl/${domain}.pem /etc/caddy/ssl/${domain}.key
 done
 
 rm -f ${domain}.key ${domain}.pem
