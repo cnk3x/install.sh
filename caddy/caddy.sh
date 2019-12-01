17784c8fac245bdd45cd36f42ad1355ef51d56f7
shu <shu@aryao.cn>
2019-12-02 02:55:33 +0800

    -

--- caddy/caddy.sh
+++ caddy/caddy.sh
@@ -3,8 +3,8 @@ set -e
 
 baseURL="https://raw.githubusercontent.com/shuxs/install.sh/master/caddy/"
 mkdir -p /etc/caddy
-wget --no-cache -O /usr/local/bin/caddy ${baseURL}/caddy
-wget --no-cache -O /etc/systemd/system/caddy.service ${baseURL}/caddy.service
-wget --no-cache -O /etc/caddy/Caddyfile ${baseURL}/Caddyfile
+wget --no-cache -nv -O /usr/local/bin/caddy ${baseURL}/caddy
+wget --no-cache -nv -O /etc/systemd/system/caddy.service ${baseURL}/caddy.service
+wget --no-cache -nv -O /etc/caddy/Caddyfile ${baseURL}/Caddyfile
 chmod +x /usr/local/bin/caddy
 systemctl daemon-reload
