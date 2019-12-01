bcf3585b03e5b4b608bd365400a08834825291ec
shu <shu@aryao.cn>
2019-12-02 02:02:35 +0800

    fix clear script

--- v2ray/clear.sh
+++ v2ray/clear.sh
@@ -5,5 +5,5 @@ systemctl disable v2ray
 rm -f /etc/systemd/system/v2ray.service
 systemctl daemon-reload
 rm -rf /etc/v2ray/
-rm -ff /usr/local/v2ray
+rm -rf /usr/local/v2ray
 rm -rf /etc/caddy/caddy.d/v2ray.caddy
