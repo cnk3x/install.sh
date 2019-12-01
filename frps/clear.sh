e32ccb8ffcaa838699403b00af13ea87eecfcb31
shu <shu@aryao.cn>
2019-12-02 03:57:31 +0800

    frps

--- frps/clear.sh
+++ frps/clear.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+systemctl stop frps
+systemctl disable frps
+
 set -e
 
 rm -rf /usr/local/bin/frps
 rm -rf /etc/frps/frps.ini
 rm -rf /etc/caddy/caddy.d/frps.caddy
 rm -rf /etc/systemd/system/frps.service
+
+systemctl daemon-reload
