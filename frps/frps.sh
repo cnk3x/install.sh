75a1c4d836eda25d692debbcdcc5aee0a764a98f
shu <shu@aryao.cn>
2019-12-02 03:44:00 +0800

    frps

--- frps/frps.sh
+++ frps/frps.sh
@@ -2,19 +2,27 @@
 
 set -e
 
+subdomain_host=$1
 token=$(openssl rand -hex 16)
 dashboard_pwd=$(openssl rand -hex 16)
-subdomain_host=$(openssl rand -hex 16)
-baseURL="https://raw.githubusercontent.com/shuxs/install.sh/master/caddy/"
-mkdir -p /etc/frps
+baseURL="https://raw.githubusercontent.com/shuxs/install.sh/master/frps/"
+
+mkdir -p /etc/frps /etc/caddy/caddy.d
 
 wget --no-cache -nv -O /usr/local/bin/frps ${baseURL}/frps
 wget --no-cache -nv -O /etc/frps/frps.ini ${baseURL}/frps.ini
+wget --no-cache -nv -O /etc/caddy/caddy.d/frps.caddy ${baseURL}/frps.caddy
 wget --no-cache -nv -O /etc/systemd/system/frps.service ${baseURL}/frps.service
 
 sed -i "s/{token}/${token}/g" /etc/frps/frps.ini
 sed -i "s/{dashboard_pwd}/${dashboard_pwd}/g" /etc/frps/frps.ini
 sed -i "s/{subdomain_host}/${subdomain_host}/g" /etc/frps/frps.ini
+sed -i "s/{subdomain_host}/${subdomain_host}/g" /etc/caddy/caddy.d/frps.caddy
 
 chmod +x /usr/local/bin/frps
 systemctl daemon-reload
+
+echo
+echo "TOKEN: ${token}"
+echo "账号: admin"
+echo "密码: ${dashboard_pwd}"
