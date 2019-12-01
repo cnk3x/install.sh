b945b4448b322a508f4a6362af586353e48975ae
shu <shu@aryao.cn>
2019-12-02 04:48:58 +0800

    do.sh

---
+++ do.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+app=$1
+case "${app}" in
+install)
+    echo "安装 ${app}"
+    wget -q -O ${app}.sh https://raw.githubusercontent.com/shuxs/install.sh/master/${app}/${app}.sh
+    sh ${app}.sh ${@:2}
+    rm -f ${app}.sh
+    ;;
+uninstall)
+    echo "卸载 ${app}"
+    wget -q -O - https://raw.githubusercontent.com/shuxs/install.sh/master/${app}/clear.sh | sh
+    ;;
+*)
+    echo "sh do.sh (v2ray | frps | caddy) (install | uninstall)"
+    ;;
+esac
