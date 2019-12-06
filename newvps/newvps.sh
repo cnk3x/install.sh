af0cf76d92fd61ffa4803037637ebcfbb7be577e
shu <2490599+shuxs@users.noreply.github.com>
2019-12-06 12:52:02 +0800

    Update newvps.sh

--- newvps/newvps.sh
+++ newvps/newvps.sh
@@ -4,6 +4,7 @@ set -e
 apt update
 apt install -y git zsh vim
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
 sed -i "s/robbyrussell/ys/g" ${HOME}/.zshrc
+chsh -s $(which zsh)
+zsh
 source ${HOME}/.zshrc
